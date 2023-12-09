993b5af5e30807b6901673c06366f5c399c3734d
ann <ann@adimian.com>
2023-12-09 02:34:31 +0100

    almostth4ere

--- day5/solution5.py
+++ day5/solution5.py
@@ -1,64 +1,128 @@
+from dataclasses import dataclass
+from functools import reduce
+
 puzzle_input = "input5.txt"
 
-maps = {
-    name: []
-    for name in [
-        "seeds",
-        "seed_to_soil_map",
-        "fertilizer_to_water_map",
-        "water_to_light_map",
-        "light_to_temperature_map",
-        "temperature_to_humidity_map",
-        "humidity_to_location_map",
-    ]
-}
+
+# I realize fully well that there is a relationship between the numbers...
+
+@dataclass
+class Map:
+    source: list[range]
+    destination: list[range]
+
+    def _find_source_range(self, number: int) -> int | None:
+        for i, rng in enumerate(self.source):
+            if number in rng:
+                return i
+        return None
+
+    def _find_index(self, which_range: int, number: int) -> int | None:
+        for j, value in enumerate(self.source[which_range]):
+            if number == value:
+                return j
+        return None
+
+    def _find_destination_value(self, which_range: int, index: int) -> int | None:
+        if which_range is not None and index is not None:
+            return self.destination[which_range][index]
+        return None
+
+    def map_number_to_destination(self, number: int) -> int:
+        which_range = self._find_source_range(number)
+        if which_range is not None:
+            index = self._find_index(which_range, number)
+            return self._find_destination_value(which_range, index)
+        else:
+            return number
 
 
-def divide_input_parts(input_data):
+def divide_input_parts(input_data) -> tuple[dict, list]:
+    maps = {
+        name: []
+        for name in [
+            "seed_to_soil_map",
+            "soil_to_fertilizer_map",
+            "fertilizer_to_water_map",
+            "water_to_light_map",
+            "light_to_temperature_map",
+            "temperature_to_humidity_map",
+            "humidity_to_location_map",
+        ]
+    }
+    seeds = []
+
     with open(input_data) as file:
+        map_iterator = iter(maps.values())
+
         for line in file:
-            map_iterator = iter(maps.values())
-            map_to_set = next(map_iterator, None)
             if line.startswith("seeds"):
                 _, seed_numbers = line.rstrip().split(": ")
-                map_to_set.append(seed_numbers)
+
+                seeds = [int(s) for s in seed_numbers.split(" ")]
+                continue
+
             elif line.strip() == "":
-                map_to_set = None
                 continue
 
-            if map_to_set is None:
-                map_to_set = next(map_iterator, None)
+            if "map" in line:
+                map_to_set = next(map_iterator)
                 continue
 
-            map_to_set.append(line.rstrip().split(" "))
+            map_to_set.append([int(n) for n in line.rstrip().split(" ")])
+
+    return maps, seeds
 
 
-def solve_part1(input_data):
-    numbers_to_add = []
-    divide_input_parts(input_data)
-    result = sum(n for n in numbers_to_add)
-    return result
+def read_range(range_: list) -> tuple[range, range]:
+    source_range_start = range_[1]
+    destination_range_start = range_[0]
+    length = range_[2]
+    source_range_stop = source_range_start + length
+    destination_range_stop = destination_range_start + length
+    decyphered_source = range(source_range_start, source_range_stop)
+    decyphered_destination = range(destination_range_start,
+                                   destination_range_stop)
+    return decyphered_source, decyphered_destination
 
 
-def total_number_of_initial_tickets(input_data):
-    with open(input_data) as file:
-        total = sum(1 for line in file)
-    return total
+def decypher_map(scrambled_map: list[list[int]]) -> Map:
+    source_points = []
+    destination_points = []
+    for part in scrambled_map:
+        decyphered_source, decyphered_destination = read_range(part)
+        source_points.append(decyphered_source)
+        destination_points.append(decyphered_destination)
+    return Map(source=source_points, destination=destination_points)
+
+
+
+def process_seed_through_maps(seed: int, maps: list[Map]) -> int:
+    return reduce(lambda number, map: map.map_number_to_destination(number)
+                                      or number,
+                  maps,
+                  seed)
+
+def solve_part1(input_data) -> int:
+    maps, seeds = divide_input_parts(input_data)
+    maps = [decypher_map(m) for m in maps.values()]
+
+    lowest: int = 0
+    for seed in seeds:
+        final_value = process_seed_through_maps(seed, maps)
+        lowest = max(lowest, final_value)
+    return lowest
 
 
 def solve_part2(input_data):
-    total_initial_tickets = total_number_of_initial_tickets(input_data)
-    numbers_to_add = []
-
-    result = sum(n for n in numbers_to_add)
-    return result
+    pass
 
 
 def generate_solution(puzzle):
     solution1 = solve_part1(puzzle)
-    solution2 = solve_part2(puzzle)
+    # solution2 = solve_part2(puzzle)
     print(f"\n\nsolution part 1 : {solution1}")
-    print(f"\nsolution part 2 : {solution2}")
+    # print(f"\nsolution part 2 : {solution2}")
 
 
 if __name__ == "__main__":
--- day5/test5.py
+++ day5/test5.py
@@ -1,7 +1,59 @@
 import pytest
-from solution5 import solve_part1, solve_part2
+from solution5 import Map, decypher_map, read_range, solve_part1, solve_part2
 
 input_data = "input5.txt"
+
+@pytest.mark.parametrize(
+    ["scrambled_part", "expected_source_range", "expected_destination_range"],
+[
+    ([50, 98, 2], range(98, 98+2), range(50, 50+2)),
+    ([52, 50, 48], range(50, 50+48), range(52, 52+48))
+])
+def test_decypher_part_of_map(scrambled_part, expected_source_range,
+                              expected_destination_range):
+    decyphered_source, decyphered_destination= read_range(scrambled_part)
+    assert decyphered_source == expected_source_range
+    assert decyphered_destination == expected_destination_range
+
+
+@pytest.mark.parametrize(
+    ["scrambled_map", "expected_decyphered_map"],
+[
+    (
+            [[50, 98, 2], [52, 50, 48]],
+            Map(
+                source=[range(98, 98+2), range(50, 50+48)],
+                destination=[range(50, 50+2), range(52, 52+48)]
+            )
+    )
+])
+def test_decypher_whole_map(scrambled_map, expected_decyphered_map):
+    decyphered_map = decypher_map(scrambled_map)
+    assert decyphered_map.source == expected_decyphered_map.source
+    assert decyphered_map.destination == expected_decyphered_map.destination
+
+
+test_map = Map(source=[range(98, 98+2), range(50, 50+48)],
+               destination=[range(50, 50+2), range(52, 52+48)])
+
+
+def map_number_to_destination(source_number, test_map):
+    pass
+
+
+@pytest.mark.parametrize(
+    ["source_number", "expected_destination_number"],
+[
+    (79, 81),
+    (14, 14),
+    (55, 57),
+    (13, 13),
+
+])
+def test_map_returns_correct_mapping_for_number(source_number,
+                                                expected_destination_number):
+    destination_number = test_map.map_number_to_destination(source_number)
+    assert destination_number == expected_destination_number
 
 
 @pytest.mark.parametrize(
