f2aec04c813fdf250dc478690508c3b647d97e18
ann <ann@adimian.com>
2023-12-14 02:41:29 +0100

    tests pass, answer wrong

--- day13/solution13.py
+++ day13/solution13.py
@@ -1,13 +1,72 @@
+import numpy as np
+from numpy.typing import NDArray
+
 puzzle_input = "input13.txt"
 
 
-def solve_part1(input_data) -> int:
+def split_input_to_maps(filename) -> list[str]:
+    with open(filename, 'r') as file:
+        content = file.read()
+    maps = content.split('\n\n')
+    return maps
+
+
+def transform_map_to_array_grid(mini_map: str) -> NDArray:
+    grid_str = mini_map.replace("#", "1").replace(".", "0").splitlines()
+    grid = [[int(char) for char in line] for line in grid_str]
+    grid = np.array(grid)
+    return grid
+
+
+def detect_vertical_mirror(map: NDArray) -> int:
+    # left side_compare
+    # from big to small
+    n_of_cols = map.shape[1]  # 2nd dimension of matix, 0-based
+
+    for cut_off_line in range(n_of_cols, 1, -1):
+        part_of_map = map[:, :cut_off_line]
+        if np.array_equal(part_of_map, np.fliplr(part_of_map)):
+            return cut_off_line // 2
+    # right side_compare
+    # from big to small
+    for cut_off_line in range(n_of_cols - 1):
+        part_of_map = map[:, cut_off_line:]
+        if np.array_equal(part_of_map, np.fliplr(part_of_map)):
+            return (n_of_cols // 2) + 1
+
     return 0
+
+
+def detect_horizontal_mirror(map: NDArray) -> int:
+    n_of_rows = map.shape[0]
+
+    for cut_off_column in range(n_of_rows, 1, -1):
+        part_of_map = map[:cut_off_column]
+        if np.array_equal(part_of_map, np.flipud(part_of_map)):
+            return cut_off_column // 2
+    for cut_off_column in range(n_of_rows - 1):
+        part_of_map = map[cut_off_column:]
+        if np.array_equal(part_of_map, np.flipud(part_of_map)):
+            return (n_of_rows // 2) + 1
+
+    return 0
+
+
+def solve_part1(input_data) -> int:
+    maps = split_input_to_maps(input_data)
+    vertical_mirrors = []
+    horizontal_mirrors = []
+    for map in maps:
+        grid = transform_map_to_array_grid(map)
+        vertical_mirrors.append(detect_vertical_mirror(grid))
+        horizontal_mirrors.append(detect_horizontal_mirror(grid))
+
+    return (sum(vertical_mirrors) +
+            sum([x * 100 for x in horizontal_mirrors]))
 
 
 def solve_part2(input_data) -> int:
     pass
-    return 0
 
 
 def generate_solution(puzzle):
--- day13/test13.py
+++ day13/test13.py
@@ -1,14 +1,90 @@
+import numpy as np
 import pytest
 
-from solution13 import solve_part1, solve_part2
+from solution13 import (
+    detect_horizontal_mirror,
+    detect_vertical_mirror,
+    solve_part1,
+    solve_part2,
+    split_input_to_maps,
+    transform_map_to_array_grid,
+)
 
 puzzle_input = "input13.txt"
+test_input = "test_input13.txt"
 
+
+def test_split_maps_into_list_of_minimaps():
+    maps = split_input_to_maps(test_input)
+    expected_first_map = ("#.##..##.\n"
+                          "..#.##.#.\n"
+                          "##......#\n"
+                          "##......#\n"
+                          "..#.##.#.\n"
+                          "..##..##.\n"
+                          "#.#.##.#.")
+    assert maps[0] == expected_first_map
+
+
+def test_transform_str_to_number_grid():
+    expected_line1 = np.array([1, 0, 1, 1, 0, 0, 1, 1, 0])
+    expected_line7 = np.array([1, 0, 1, 0, 1, 1, 0, 1, 0])
+
+    maps = split_input_to_maps(test_input)
+
+    grid = transform_map_to_array_grid(maps[0])
+    assert np.array_equal(grid[0], expected_line1)
+    assert np.array_equal(grid[6], expected_line7)
+
+
+@pytest.mark.parametrize("simple_line, expected_mirror", [
+    [np.array([[1, 0, 0, 1, 1]]), 2],
+    [np.array([[2, 1, 0, 0, 1]]), 3],
+    [np.array([[0, 0, 1]]), 1],
+    [np.array([[0, 3, 4, 2]]), 0],  # representation of != columns
+])
+def test_detect_vertical_mirror_simple(simple_line, expected_mirror):
+    assert detect_vertical_mirror(simple_line) == expected_mirror
+
+
+maps = split_input_to_maps(test_input)
+grid1 = transform_map_to_array_grid(maps[0])
+grid2 = transform_map_to_array_grid(maps[1])
+
+
+def test_detect_vertical_mirror_grid1():
+    expected_mirror_line_map1 = 5
+    assert detect_vertical_mirror(grid1) == expected_mirror_line_map1
+
+
+def test_detect_vertical_mirror_grid2():
+    expected_mirror_line_map2 = 0
+    assert detect_vertical_mirror(grid2) == expected_mirror_line_map2
+
+
+@pytest.mark.parametrize("simple_column, expected_mirror", [
+    [np.array([[1], [0], [0], [1], [1]]), 2],
+    [np.array([[2], [1], [0], [0], [1]]), 3],  # to avoid 2 mirrors
+    [np.array([[0], [0], [1]]), 1],
+    [np.array([[0], [2], [1], [4]]), 0]
+])
+def test_detect_horizontal_mirror_simple(simple_column, expected_mirror):
+    assert detect_horizontal_mirror(simple_column) == expected_mirror
+
+
+def test_detect_horizontal_mirror_grid1():
+    expected_mirror_line_map1 = 0
+    assert detect_horizontal_mirror(grid1) == expected_mirror_line_map1
+
+
+def test_detect_horizontal_mirror_grid2():
+    expected_mirror_line_map2 = 4
+    assert detect_horizontal_mirror(grid2) == expected_mirror_line_map2
 
 @pytest.mark.parametrize(
     "part, input_data, expected_output",
     [
-        (1, puzzle_input, 405),
+        (1, test_input, 405),
         # (2, "test_input7.txt", 0),
     ],
 )
